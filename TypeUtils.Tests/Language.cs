6c810db7f485ffe1dbd1d48adb83a4039542cf97
Millzy <millzyyt08@gmail.com>
2022-03-09 16:11:06 +0000

    test

---
+++ TypeUtils.Tests/Language.cs
@@ -0,0 +1,12 @@
+﻿using TypeUtils.Enumerator;
+using Value = System.Tuple<string, string>;
+
+namespace TypeUtils.Tests
+{
+    internal class Language : FEnum<Value>
+    {
+        public static readonly FEnum<Value>.FValue EN = new Value("English", "EN");
+        public static readonly FEnum<Value>.FValue FR = new Value("French", "FR");
+        public static readonly FEnum<Value>.FValue ES = new Value("Spanish", "ES");
+    }
+}
---
+++ TypeUtils.Tests/Program.cs
@@ -0,0 +1,28 @@
+﻿using fint = TypeUtils.Integer.FInt32;
+using fshort = TypeUtils.Integer.FInt16;
+
+namespace TypeUtils.Tests
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Tuple<string, string> french = Language.FR;
+            Console.WriteLine($"Language: {french.Item1}, Code: {french.Item2}");
+
+            fint i = new(0, min: 0, max: 5);
+            int j = 0;
+            while (j < 12)
+            {
+                i++;
+                j++;
+                Console.WriteLine(i);
+            }
+
+            fshort x = new(6, min: 0, max: 5);
+            Console.WriteLine($"Overflow FInt16: {x}");
+            fshort y = new(-1, min: 0, max: 5);
+            Console.WriteLine($"Underflow FInt16: {y}");
+        }
+    }
+}
