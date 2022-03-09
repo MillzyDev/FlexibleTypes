e7583790d14f1d340b3d987a531eb38b705f3268
Millzy <millzyyt08@gmail.com>
2022-03-09 17:41:02 +0000

    add test

--- TypeUtils.Tests/Program.cs
+++ TypeUtils.Tests/Program.cs
@@ -8,7 +8,7 @@ namespace FlexibleTypes.Tests
         {
             Tuple<string, string> french = Language.FR;
             Console.WriteLine($"Language: {french.Item1}, Code: {french.Item2}");
-
+            Console.WriteLine("Integer Overflow Test:");
             fint i = new(0, min: 0, max: 5);
             int j = 0;
             while (j < 12)
